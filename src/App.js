abdb13c6e95f9a2c3021ea22194f19acabadcb3d
Josh Waggoner <josh@passitdown.com>
2022-08-15 18:20:37 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+// we created a component called "App"
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", { id: "brand" }, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+      name: "Doink",
+      animal: "Cat",
+      breed: "Mix",
+    }),
+  ]);
+};
+
+// App here is like an object. It's "an" App that createElement makes
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
