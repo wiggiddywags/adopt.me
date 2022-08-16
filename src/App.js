6484de276ff2e73a12d73aaea35d06bafc2d8b23
Josh Waggoner <josh@passitdown.com>
2022-08-16 14:13:50 -0400

    hello JSX

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-// we created a component called "App"
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import React from "react";
+// import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import Pet from "./Pet";
 
+// we created a component called "App"
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", { id: "brand" }, "Adopt Me!"),
@@ -29,4 +26,4 @@ const App = () => {
 };
 
 // App here is like an object. It's "an" App that createElement makes
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
